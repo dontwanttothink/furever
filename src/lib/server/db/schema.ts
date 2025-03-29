4312740a53c29872192a678e313c4daeeb5ad749
idontwanttothink <barquean.09excavacion@icloud.com>
2025-03-29 15:32:55 -0500

    Add a schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,2 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+export const usersTable = sqliteTable("users_table", {
+	id: int().primaryKey({ autoIncrement: true }),
+	name: text().notNull(),
+	email: text().notNull().unique(),
+	passwordData: text().notNull(),
+});
