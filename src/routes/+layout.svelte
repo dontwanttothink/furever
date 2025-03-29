c9792fef626e0fbd18909edabf42c1b0e292ff86
idontwanttothink <barquean.09excavacion@icloud.com>
2025-03-29 13:59:33 -0500

    Fix navigation flicker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import { assets } from "$app/paths";
+
 	const { data, children } = $props();
 </script>
+
+<sveltekit:head>
+	<link rel="preload" href="{assets}/centered.css" as="style" />
+</sveltekit:head>
 
 <nav>
 	{#each data?.routes as route (route.path)}
