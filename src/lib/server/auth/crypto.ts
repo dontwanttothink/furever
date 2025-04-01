4c600c5d5323afee54f18fdd7a0abdb746b9aa23
idontwanttothink <barquean.09excavacion@icloud.com>
2025-03-31 19:04:23 -0500

    Remove unused crypto file

--- src/lib/server/auth/crypto.ts
+++
@@ -1,6 +0,0 @@
-import { PASSWORD_DATA_SEPARATOR } from "./internal";
-
-export function checkPassword(given: string, stored: string) {
-	const [salt, hash] = stored.split(PASSWORD_DATA_SEPARATOR);
-	const saltedGiven = given + salt;
-}
