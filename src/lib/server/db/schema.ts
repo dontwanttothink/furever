6acb156062d4f73f2d09fb33a79b31f3d924fc8e
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-09 18:34:47 -0500

    Add indexing

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,16 +1,24 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
-export const usersTable = sqliteTable("users_table", {
-	id: int().primaryKey({ autoIncrement: true }),
-	name: text().notNull(),
-	email: text().notNull().unique(),
-	passwordData: text().notNull(),
-});
+import { index, int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+export const usersTable = sqliteTable(
+	"users_table",
+	{
+		id: int().primaryKey({ autoIncrement: true }),
+		name: text().notNull(),
+		email: text().notNull().unique(),
+		passwordData: text().notNull(),
+	},
+	(table) => [index("email_idx").on(table.email)],
+);
 
-export const sessionsTable = sqliteTable("sessions_table", {
-	id: int().primaryKey({ autoIncrement: true }),
-	userId: int()
-		.notNull()
-		.references(() => usersTable.id),
-	token: text().notNull().unique(),
-	expiresAt: int().notNull(),
-});
+export const sessionsTable = sqliteTable(
+	"sessions_table",
+	{
+		id: int().primaryKey({ autoIncrement: true }),
+		userId: int()
+			.notNull()
+			.references(() => usersTable.id),
+		token: text().notNull().unique(),
+		expiresAt: int().notNull(),
+	},
+	(table) => [index("expires_at_idx").on(table.expiresAt)],
+);
