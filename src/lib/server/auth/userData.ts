f0a40e5dc1302e98f8a453566c6fc06c71036c12
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-11 14:59:40 -0500

    Implement getUserDataFor function to retrieve user data based on session token

---
+++ src/lib/server/auth/userData.ts
@@ -0,0 +1,37 @@
+import { assert } from "$lib";
+import { eq } from "drizzle-orm";
+import { db, sessionsTable, usersTable } from "../db";
+
+class InvalidSessionError extends Error {}
+
+/**
+ * Get user data for a given session.
+ * @throws UserDoesNotExistError
+ */
+export async function getUserDataFor(sessionToken: string) {
+	const matches = await db
+		.select({
+			userId: sessionsTable.userId,
+		})
+		.from(sessionsTable)
+		.where(eq(sessionsTable.token, sessionToken));
+
+	if (matches.length > 1) {
+		throw new Error(
+			"More than one session appears to have the token: " + sessionToken,
+		);
+	}
+	if (matches.length == 0) {
+		throw new InvalidSessionError();
+	}
+
+	const userId = matches[0].userId;
+	const userMatches = await db
+		.select()
+		.from(usersTable)
+		.where(eq(usersTable.id, userId));
+
+	assert(userMatches.length == 1);
+
+	return userMatches[0];
+}
