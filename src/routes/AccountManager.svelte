6c32afb463deb6ccc34921d1dabee8315740dc80
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-11 13:34:37 -0500

    Indicate open state more clearly

--- src/routes/AccountManager.svelte
+++ src/routes/AccountManager.svelte
@@ -12,12 +12,13 @@
 {#if token}
 	<div class="dropdown">
 		<button
-			class="dropdown-toggle"
+			class={["dropdown-toggle", showDropdown ? "expanded" : ""]}
 			tabindex="0"
 			onclick={toggleDropdown}
 			onkeydown={toggleDropdown}
 		>
 			{token.slice(0, 10)}…
+			<!-- ^^^ FIXME -->
 		</button>
 		{#if showDropdown}
 			<ul
@@ -30,7 +31,7 @@
 						<input type="submit" value="Cerrar sesión" />
 					</form>
 				</li>
-				<li role="menuitem"><a href="">Ajustes</a></li>
+				<li role="menuitem"><a href="/ajustes">Ajustes</a></li>
 			</ul>
 		{/if}
 	</div>
@@ -39,43 +40,6 @@
 {/if}
 
 <style>
-	p {
-		text-align: center;
-	}
-	.dropdown {
-		position: relative;
-		user-select: none;
-	}
-	.dropdown-toggle:hover {
-		cursor: pointer;
-	}
-	.dropdown-items {
-		position: absolute;
-		display: block;
-		background-color: rgb(238, 238, 238);
-		padding: 0.5rem;
-		border-radius: 10px;
-		list-style: none;
-		top: 50%;
-		right: 0;
-		width: max-content;
-		/* Shadow */
-		box-shadow: 0 5px 5px rgba(0, 0, 0, 0.05);
-	}
-
-	/* Style the input type submit so that it looks like the rest of the items */
-	input[type="submit"] {
-		background-color: transparent;
-		border: none;
-		padding: 0;
-		text-align: left;
-		font-size: inherit;
-		cursor: pointer;
-	}
-	input[type="submit"]:hover {
-		text-decoration: underline;
-	}
-
 	button {
 		background-color: unset;
 		transition: unset;
@@ -85,8 +49,64 @@
 		transform: unset;
 		text-align: center;
 	}
+
+	p {
+		text-align: center;
+	}
+
 	a,
-	button {
+	button,
+	input[type="submit"] {
 		font-weight: 500;
+		color: unset;
+	}
+
+	li,
+	input[type="submit"] {
+		font-size: 0.8rem;
+		margin: 0;
+		padding: 0;
+	}
+
+	.dropdown {
+		position: relative;
+		user-select: none;
+	}
+	.dropdown-toggle {
+		padding: 0.2rem;
+		transition: background-color 150ms ease;
+		transition: transform 150ms ease;
+	}
+	.dropdown-toggle.expanded {
+		background-color: rgb(230, 230, 230);
+		transform: scale(110%);
+	}
+	.dropdown-toggle:hover {
+		cursor: pointer;
+	}
+	.dropdown-items {
+		position: absolute;
+		display: block;
+		background-color: rgb(238, 238, 238);
+		padding: 0.3em;
+		border-radius: 10px;
+		list-style: none;
+		top: 50%;
+		right: 0;
+		width: max-content;
+		/* Shadow */
+		box-shadow: 0 5px 5px rgba(0, 0, 0, 0.05);
+	}
+
+	/* Style the submit input so that it looks like the rest of the items */
+	input[type="submit"] {
+		background-color: transparent;
+		border: none;
+		padding: 0;
+		text-align: left;
+		cursor: pointer;
+	}
+	input[type="submit"]:hover {
+		text-decoration: underline;
 	}
 </style>
