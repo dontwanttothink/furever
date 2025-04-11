db3e149afc7d0ea73524937b97d819458115579b
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-11 17:56:11 -0500

    Add privacy policy page with content on data collection, usage, and protection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,6 +23,15 @@
 	{@render children()}
 </main>
 
+<footer>
+	<p>
+		<span id="copy">Copyright &copy; 2025</span> <br />santi_nubuwu y los
+		colaboradores de FureverHome
+	</p>
+
+	<p><a href="/privacidad">Información sobre la privacidad</a></p>
+</footer>
+
 <style>
 	header {
 		display: flex;
@@ -30,5 +39,21 @@
 		flex-direction: row;
 		align-items: center;
 		justify-content: space-between;
+	}
+
+	footer {
+		border-top: 1px solid rgb(0 0 0 / 7%);
+		margin-top: 3rem;
+		font-size: 0.9rem;
+		text-align: center;
+		font-weight: 300;
+
+		a {
+			font-weight: 400;
+		}
+	}
+
+	#copy {
+		font-weight: 500;
 	}
 </style>
---
+++ src/routes/privacidad/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { titlePrefix } from "$lib/misc";
+
+	const title = "Política de privacidad";
+</script>
+
+<svelte:head>
+	<title>{titlePrefix}{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+<p>
+	Bienvenido a nuestra política de privacidad. Aquí explicamos cómo recopilamos,
+	usamos y protegemos su información personal.
+</p>
+<section>
+	<h2>Recopilación de información</h2>
+	<p>
+		Detallamos los datos que recopilamos y cómo los utilizamos para mejorar
+		nuestros servicios.
+	</p>
+</section>
+<section>
+	<h2>Uso de la información</h2>
+	<p>
+		Explicamos cómo utilizamos su información para personalizar su experiencia y
+		garantizar la seguridad.
+	</p>
+</section>
+<section>
+	<h2>Protección de datos</h2>
+	<p>
+		Describimos las medidas que tomamos para proteger su información personal.
+	</p>
+</section>
