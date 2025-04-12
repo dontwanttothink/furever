a7cd371ae52d0fb9e8a3ffe36fc413e62802d0b3
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-12 18:48:08 -0500

    Add content to privacy policy

--- src/routes/privacidad/+page.svelte
+++ src/routes/privacidad/+page.svelte
@@ -10,26 +10,52 @@
 
 <h1>{title}</h1>
 <p>
-	Bienvenido a nuestra política de privacidad. Aquí explicamos cómo recopilamos,
-	usamos y protegemos su información personal.
+	En esta política de privacidad, explicamos cómo recopilamos, usamos y
+	compartimos tu información personal.
 </p>
 <section>
-	<h2>Recopilación de información</h2>
+	<h2>Datos que recopilamos</h2>
+	<p>En FureverHome, recopilamos los siguientes datos:</p>
+	<ul>
+		<li>El nombre que ingreses en el formulario de registro.</li>
+		<li>Tu dirección de correo electrónico.</li>
+		<li>Tu dirección IP en forma anonimizada, conocida como hash.</li>
+		<li>
+			Cualquier contenido que decidas compartir en la plataforma, como imágenes,
+			datos de mascotas o texto.
+		</li>
+	</ul>
+</section>
+<section>
+	<h2>Uso de los datos</h2>
 	<p>
-		Detallamos los datos que recopilamos y cómo los utilizamos para mejorar
-		nuestros servicios.
+		Usamos tu nombre y dirección de correo electrónico para identificarte y
+		asegurarnos de que solo tú puedas acceder a tu cuenta. Los hashes de
+		direcciones IP se recopilan por motivos de seguridad, como establecer
+		límites de frecuencia de solicitudes para mantener el buen rendimiento de la
+		plataforma para todos los usuarios. Finalmente, cualquier contenido que
+		publiques se recopila para ponerlo a disposición de otros usuarios.
 	</p>
 </section>
 <section>
-	<h2>Uso de la información</h2>
+	<h2>Cómo compartimos tus datos</h2>
 	<p>
-		Explicamos cómo utilizamos su información para personalizar su experiencia y
-		garantizar la seguridad.
+		En FureverHome, no compartimos tu información personal con terceros, excepto
+		en los siguientes casos:
 	</p>
-</section>
-<section>
-	<h2>Protección de datos</h2>
-	<p>
-		Describimos las medidas que tomamos para proteger su información personal.
-	</p>
+	<ul>
+		<li>
+			Cuando sea requerido por ley o en respuesta a un proceso legal válido.
+		</li>
+		<li>
+			Cuando utilizamos servicios de terceros, como infraestructura en la nube o
+			redes de distribución de contenido (CDN) como Cloudflare, para operar y
+			asegurar nuestra plataforma. Estos servicios pueden procesar tus datos
+			únicamente para los fines necesarios para proporcionar sus servicios.
+		</li>
+		<li>
+			En caso de una fusión, adquisición o venta de activos, donde tus datos
+			podrían transferirse como parte del acuerdo.
+		</li>
+	</ul>
 </section>
