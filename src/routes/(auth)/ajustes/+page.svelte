845aaed99f528484e8e56e19d9a5bf7bdae6ae10
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-12 19:05:44 -0500

    Add a basic template for the future settings page

---
+++ src/routes/(auth)/ajustes/+page.svelte
@@ -0,0 +1,21 @@
+<!-- TODO: Add Settings. It should enable people to change their password,
+their name, or delete their account. -->
+
+<script lang="ts">
+</script>
+
+<div class="settings">
+	<h1>Configuración de la cuenta</h1>
+
+	<section>
+		<h2>Cambiar nombre</h2>
+	</section>
+
+	<section>
+		<h2>Cambiar contraseña</h2>
+	</section>
+
+	<section>
+		<h2>Eliminar cuenta</h2>
+	</section>
+</div>
