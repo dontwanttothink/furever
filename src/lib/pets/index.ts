15a014a829c724f433a935a8dadcb39545be8d69
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-23 02:01:18 -0500

    Export functions to get user readable names

--- src/lib/pets/index.ts
+++ src/lib/pets/index.ts
@@ -5,10 +5,34 @@ export enum Sex {
 	Indeterminate,
 }
 
+export function getUserReadableSexName(sex: Sex): string {
+	switch (sex) {
+		case Sex.Unknown:
+			return "Desconocido";
+		case Sex.Male:
+			return "Masculino";
+		case Sex.Female:
+			return "Femenino";
+		case Sex.Indeterminate:
+			return "Indeterminado u ambiguo";
+	}
+}
+
 export enum Species {
 	Other = 0,
 	Dog = 1,
 	Cat = 2,
+}
+
+export function getUserReadableSpeciesName(species: Species): string {
+	switch (species) {
+		case Species.Cat:
+			return "Gato";
+		case Species.Dog:
+			return "Perro";
+		case Species.Other:
+			return "Otra especie";
+	}
 }
 
 export enum DogBreeds {
@@ -26,6 +50,39 @@ export enum DogBreeds {
 	YorkshireTerrier,
 	Boxer,
 	Dachshund,
+}
+
+export function getUserReadableBreedName(breed: DogBreeds): string {
+	switch (breed) {
+		case DogBreeds.Other:
+			return "Otra raza";
+		case DogBreeds.Mixed:
+			return "Mestizo";
+		case DogBreeds.Unknown:
+			return "Ninguna especificada";
+		case DogBreeds.BorderCollie:
+			return "Border Collie";
+		case DogBreeds.Labrador:
+			return "Labrador";
+		case DogBreeds.GermanShepherd:
+			return "Pastor Alemán";
+		case DogBreeds.GoldenRetriever:
+			return "Golden Retriever";
+		case DogBreeds.Bulldog:
+			return "Bulldog";
+		case DogBreeds.Beagle:
+			return "Beagle";
+		case DogBreeds.Poodle:
+			return "Poodle";
+		case DogBreeds.Rottweiler:
+			return "Rottweiler";
+		case DogBreeds.YorkshireTerrier:
+			return "Yorkshire Terrier";
+		case DogBreeds.Boxer:
+			return "Boxer";
+		case DogBreeds.Dachshund:
+			return "Dachshund";
+	}
 }
 
 export enum CatBreeds {
