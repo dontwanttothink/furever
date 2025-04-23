c12fcc1ea49db30e11181affc26d32c3a5ac7549
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-23 02:03:00 -0500

    Add author name information

--- src/routes/mascotas/+page.server.ts
+++ src/routes/mascotas/+page.server.ts
@@ -1,14 +1,25 @@
 import { db } from "$lib/server/db";
-import { desc } from "drizzle-orm";
+import { desc, type InferSelectModel } from "drizzle-orm";
 import { petsTable } from "$lib/server/db/schema";
+import { getUserDataById } from "$lib/server/auth/userData";
 
 export async function load() {
 	// Fetch recent pets from database
-	const recentPets = await db
-		.select()
-		.from(petsTable)
-		.orderBy(desc(petsTable.timestamp))
-		.limit(10);
+	const recentPets: (InferSelectModel<typeof petsTable> & {
+		authorName: string;
+	})[] = await Promise.all(
+		(
+			await db
+				.select()
+				.from(petsTable)
+				.orderBy(desc(petsTable.timestamp))
+				.limit(10)
+		).map(async (pet) => {
+			const authorId = pet.author;
+			const { name } = await getUserDataById(authorId);
+			return { ...pet, authorName: name };
+		}),
+	);
 
 	return {
 		recentPets,
