92e453876c5b7fd99c4768b0e69acc71b4cc3937
idontwanttothink <barquean.09excavacion@icloud.com>
2025-04-27 15:17:25 -0500

    Add some content and functionality to settings page

---
+++ src/routes/(auth)/ajustes/+page.server.ts
@@ -0,0 +1,53 @@
+import { error, fail, type Actions } from "@sveltejs/kit";
+import { updateName } from "$lib/server/auth";
+import { getUserDataByToken } from "$lib/server/auth";
+import {
+	InvalidSessionError,
+	InvalidUserError,
+} from "$lib/server/auth/errors.js";
+import { tidyName } from "$lib/hygiene";
+
+export async function load({ parent }) {
+	const parentData = await parent();
+	if (!parentData.userData) {
+		error(401, "No estás autenticado.");
+	}
+	return {
+		userData: parentData.userData, // just for typescript to understand
+	};
+}
+
+export const actions = {
+	async name({ request, cookies }) {
+		const token = cookies.get("secret_token");
+		if (!token) {
+			return fail(401, { error: "Se necesita un token." });
+		}
+
+		const data = await request.formData();
+		const newName = data.get("name")?.toString();
+
+		if (!newName) {
+			return fail(400, { error: "Se necesita el nuevo nombre." });
+		}
+
+		let userId;
+		try {
+			userId = (await getUserDataByToken(token)).userId;
+		} catch (e) {
+			if (e instanceof InvalidSessionError) {
+				return fail(400, { error: "La sesión es inválida." });
+			}
+			throw e;
+		}
+
+		try {
+			await updateName(userId, tidyName(newName));
+		} catch (e) {
+			if (e instanceof InvalidUserError) {
+				return fail(400, { error: "Parece que el usuario ya no existe." });
+			}
+			throw e;
+		}
+	},
+} satisfies Actions;
--- src/routes/(auth)/ajustes/+page.svelte
+++ src/routes/(auth)/ajustes/+page.svelte
@@ -1,21 +1,89 @@
-<!-- TODO: Add Settings. It should enable people to change their password,
-their name, or delete their account. -->
-
 <script lang="ts">
+	import { titlePrefix, contactEmail } from "$lib";
+
+	const { data } = $props();
+	const { userData } = data;
 </script>
+
+<svelte:head>
+	<title>{titlePrefix}Ajustes de cuenta</title>
+</svelte:head>
 
 <div class="settings">
 	<h1>Configuración de la cuenta</h1>
 
 	<section>
-		<h2>Cambiar nombre</h2>
+		<form action="?/name" method="POST">
+			<h2>Cambiar nombre</h2>
+			<p>
+				El nombre que des se mostrara públicamente, incluido cuando envíes
+				mensajes o registres mascotas.
+			</p>
+			<div id="text-et-button">
+				<input
+					type="text"
+					id="name"
+					name="name"
+					placeholder={userData.name}
+					required
+				/>
+				<button type="submit">Actualizar</button>
+			</div>
+		</form>
 	</section>
 
 	<section>
-		<h2>Cambiar contraseña</h2>
+		<form action="?/changePassword" method="POST">
+			<h2>Cambiar contraseña</h2>
+			<p>
+				Cambiar la contraseña no es posible todavía. Entendemos que es una
+				omisión importante.
+			</p>
+		</form>
 	</section>
 
 	<section>
-		<h2>Eliminar cuenta</h2>
+		<form action="?/deleteAccount" method="POST">
+			<h2>Eliminar cuenta</h2>
+			<div>
+				<p>
+					Tienes derecho a eliminar tu cuenta, incluida toda la información
+					asociada que esté almacenada. Por favor, contacta a <a
+						href="mailto:{contactEmail}">{contactEmail}</a
+					>.
+				</p>
+				<small
+					>No es necesario dar ninguna justificación. Será posible eliminar la
+					cuenta desde el navegador en el futuro próximo.</small
+				>
+			</div>
+		</form>
 	</section>
 </div>
+
+<style>
+	#text-et-button {
+		display: flex;
+		flex-direction: row;
+		gap: 1em;
+		align-items: center;
+	}
+
+	section {
+		width: fit-content;
+		margin: 0 auto;
+		margin-bottom: 0.6rem;
+
+		> form {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			max-width: 30rem;
+			gap: 0;
+
+			h2 {
+				margin-bottom: 0.5rem;
+			}
+		}
+	}
+</style>
