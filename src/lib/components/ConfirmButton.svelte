ce698dfe0c8d3bcd2dceffab26883872c63e96d7
idontwanttothink <barquean.09excavacion@icloud.com>
2025-05-10 15:06:06 -0500

    Add ConfirmButton component with modal functionality

---
+++ src/lib/components/ConfirmButton.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { assert } from "$lib";
+
+	const { children, message } = $props();
+	let modal: HTMLDialogElement | null = $state(null);
+
+	export function cancel() {
+		assert(modal instanceof HTMLDialogElement);
+		modal.close();
+	}
+</script>
+
+<dialog bind:this={modal}>
+	{@render children()}
+</dialog>
+<button
+	onclick={() => {
+		assert(modal instanceof HTMLDialogElement);
+		modal.showModal();
+	}}>{message}…</button
+>
