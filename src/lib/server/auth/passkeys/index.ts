aa0e95482e04a244c7221494ab5539f3b59e765b
idontwanttothink <barquean.09excavacion@icloud.com>
2025-05-15 18:05:04 -0500

    Use stated origin for passkeys

--- src/lib/server/auth/passkeys/index.ts
+++ src/lib/server/auth/passkeys/index.ts
@@ -1,5 +1,7 @@
+import { env } from "$env/dynamic/private";
+
 export const signingPair = await crypto.subtle.generateKey("Ed25519", false, [
 	"sign",
 	"verify",
 ]);
-export const rpID = "furever.uq4.net";
+export const rpID = new URL(env.ORIGIN).hostname; // removes port too (required)
