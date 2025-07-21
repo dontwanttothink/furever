e1feaa8910723507d8538cbb9effd39c89e22e11
idontwanttothink <barquean.09excavacion@icloud.com>
2025-07-21 15:59:40 -0500

    Remove duplicated script

--- Web/bun_verify.sh
+++
@@ -1,9 +0,0 @@
-case "$(which node 2>/dev/null)" in
-  *bun-node*)
-    ;;  # do nothing
-  *)
-    printf "\e[1m\e[33mWarning: Run commands with \"bun run --bun <command>\".\e[0m\n"
-    echo "Giving you a moment to cancel…"
-    sleep 1.7
-    ;;
-esac
