dcac6c31c66ee366a6ee719d1f12235311db10cd
idontwanttothink <barquean.09excavacion@icloud.com>
2025-07-21 21:18:09 -0500

    Fix horrible frustrating crash

--- Web/src/routes/mascotas/imágenes/[uuid]/+server.ts
+++ Web/src/routes/mascotas/imágenes/[uuid]/+server.ts
@@ -15,7 +15,38 @@ export const GET: RequestHandler = async ({ params: { uuid }, platform }) => {
 	}
 
 	const headers = new Headers();
-	image.writeHttpMetadata(headers as unknown as Worker.Headers);
+
+	// image.writeHttpMetadata(headers); for some reason this throws ????????????
+	// which really frustrated me. i hate computers.
+	//
+	// my typescript language server was also using the wrong fucking tsconfig
+	// all the time and thinking i was on node, so i had to switch language servers.
+	//
+	// God take me
+
+	const httpMetadataHeaderNames = new Map([
+		["contentType", "content-type"],
+		["contentLanguage", "content-language"],
+		["contentDisposition", "content-disposition"],
+		["contentEncoding", "content-encoding"],
+		["cacheControl", "cache-control"],
+		["cacheExpiry", "cache-expiry"],
+	]);
+	if (image.httpMetadata) {
+		for (const [property, value] of Object.entries(image.httpMetadata)) {
+			const name = httpMetadataHeaderNames.get(property);
+
+			if (!name) {
+				console.warn(
+					`While serving an R2 image (${uuid}),\n` +
+						`    unrecognized \`httpMetadata\` property: ${property}`,
+				);
+				continue;
+			}
+			headers.set(name, value);
+		}
+	}
+
 	headers.set("etag", image.httpEtag);
 
 	return new Response(image.body as unknown as ReadableStream, {
